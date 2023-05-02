8571e4904752d6bcea84026c294b4315f25894f1
faiwzz <volkovailona18@gmail.com>
2023-05-02 15:26:16 +0300

    added hw for lesson1

---
+++ lesson1/task1.js
@@ -0,0 +1,47 @@
+let hello = 'hello';
+console.log('hello');
+let owu = 'owu';
+console.log('owu');
+let com = 'com';
+console.log('com');
+let ua = 'ua';
+console.log('ua');
+let q = 1;
+console.log(q);
+let w = 10;
+console.log(w);
+let r = -999;
+console.log(r);
+let d = 123;
+console.log(d);
+let e = 3.14;
+console.log(e);
+let f = 2.7;
+console.log(f);
+let g = 16;
+console.log(g);
+let check1 = true;
+console.log(check1);
+let check2 = false;
+console.log(check2);
+
+let firstName = 'Ilona';
+let middleName = 'Yuriivna';
+let lastName = 'Volkova';
+
+let user = `${lastName} ${firstName} ${middleName}`
+console.log(user);
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+
+let name = prompt()
+console.log(name);
+let midname = prompt();
+console.log(midname);
+let age = prompt();
+console.log(age);
